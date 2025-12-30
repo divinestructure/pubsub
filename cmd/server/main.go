7acf400725af8fa6d65d5f3e728ff4c035196a1b
github_divinestructure <finov0205@gmail.com>
2025-12-30 14:29:49 +0300

    PublishJSON and exchange created with UI from docker

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -13,11 +15,18 @@ func main() {
 	const connectionString = "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connectionString)
 	if err != nil {
-		log.Fatalf("error establishing connection %v", err)
+		log.Fatalf("error establishing connection: %v", err)
 	}
 	defer conn.Close()
-
 	fmt.Println("Successful connection to RabbitMQ")
+
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("error opening channel: %v", err)
+	}
+	pubsub.PublishJSON(publishCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
---
+++ internal/pubsub/publish.go
@@ -0,0 +1,26 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	jsonVal, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("marshaling error: %v", err)
+	}
+
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        jsonVal,
+	})
+	if err != nil {
+		return fmt.Errorf("publishing errorr: %v", err)
+	}
+
+	return nil
+}
