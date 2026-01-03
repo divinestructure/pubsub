a83c00b1994d69bf4b68c4321388e00759926d32
github_divinestructure <finov0205@gmail.com>
2026-01-03 15:54:13 +0300

    publishing-exchange-queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -24,6 +24,13 @@ func main() {
 		log.Fatalf("error opening channel: %v", err)
 	}
 
+	_, queue, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilTopic, routing.GameLogSlug, routing.GameLogSlug+".*", pubsub.SimpleQueueDurable)
+	if err != nil {
+		log.Fatalf("could not subscribe to pause: %v", err)
+	}
+
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
 	gamelogic.PrintServerHelp()
 
 	for {
